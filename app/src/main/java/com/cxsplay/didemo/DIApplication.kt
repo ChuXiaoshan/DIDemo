1d400c033eeebfaf8bbea6980013de26b7e0cc2f
XiaoshanChu <Xiaoshan1215@gmail.com>
2022-08-24 22:58:23 +0800

    init1

---
+++ app/src/main/java/com/cxsplay/didemo/DIApplication.kt
@@ -0,0 +1,12 @@
+package com.cxsplay.didemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by xiaoshanchu on 2022/8/24
+ *
+ * Description:
+ */
+@HiltAndroidApp
+class DIApplication : Application()
